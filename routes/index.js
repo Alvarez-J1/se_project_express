d773e239bddd6bf31cec6a01156820843738eee4
Joel Alvarez <Joel124@outlook.es>
2025-07-15 17:56:05 -0400

    second submission third try

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const router = require("express").Router();
 
 const itemRouter = require("./clothingItems");
 
+const { createUser, login } = require("../controllers/users");
+
 const userRouter = require("./users");
 
 router.use("/users", userRouter);
 router.use("/items", itemRouter);
-
+router.post("/signin", login);
+router.post("/signup", createUser);
 module.exports = router;
